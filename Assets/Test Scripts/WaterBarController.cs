0494bbfb24c5b6d2a66fe36354b7a803ddc50ffd
Aidan M <aidansmcmurry@gmail.com>
2023-04-20 10:47:17 -0500

    Added Timer pickups and coded UI pickup bar

---
+++ Assets/Test Scripts/WaterBarController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterBarController : MonoBehaviour
+{
+    [SerializeField]
+    Slider waterBar;
+    [SerializeField]
+    Text waterText;
+
+    public float maxCap = 100;
+    public float curCap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curCap = waterBar.value;
+        
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Pickup")
+        {
+            waterBar.value += 10f;
+            curCap = waterBar.value;
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
